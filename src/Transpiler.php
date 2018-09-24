90feec75a78d1bcce1b23a2ab8cb88c473d82d52
Angelo Giuffredi <agiuffredi@gmail.com>
2018-09-24 20:28:27 +0200

    First commit

---
+++ src/Transpiler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: angelogiuffredi
+ * Date: 9/24/18
+ * Time: 8:18 PM
+ */
+
+class Transpiler
+{
+    public function transpile(): void
+    {
+
+    }
+}
